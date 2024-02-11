85aa13f538a2aa3b7be72d1935aebaa7b360dd80
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-02-11 16:41:41 +0500

    secure login flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
-      <body className={cn('min-h-screen font-sans antialiased grainy')}>{children}</body>
+    <html lang="en" className="">
+      <body className={cn("min-h-screen font-sans antialiased grainy")}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
