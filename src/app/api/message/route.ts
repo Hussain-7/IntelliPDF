d08c17970e85ee609f4085ab07392edb7a27ddc7
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-27 01:54:21 +0500

    send message and response api route created

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,7 +1,12 @@
 import { db } from "@/db";
+import { pinecone } from "@/lib/pinecone";
 import { SendMessageValidator } from "@/lib/validators/sendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
 import { NextRequest } from "next/server";
+import { openai } from "@/lib/openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -21,8 +26,8 @@ export const POST = async (req: NextRequest) => {
       userId: user.id,
     },
   });
-  if (!file)
-    new Response("Not Found", {
+  if (!file || !file?.id)
+    return new Response("Not Found", {
       status: 404,
     });
 
@@ -33,7 +38,90 @@ export const POST = async (req: NextRequest) => {
       userId: user.id,
       isUserMessage: true,
     },
-	});
-	
-	// Use some LLM here for answering the question
+  });
+
+  // Use some LLM here for answering the question
+
+  const pineconeIndex = pinecone.index("intelli-pdf");
+  console.log("Index found");
+
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPEN_AI_API_KEY!,
+  });
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  // Checking for 4 closest results to the message
+  const results = await vectorStore.similaritySearch(message, 4);
+
+  console.log("Search results from vector db", results);
+
+  // also pass previous messages to the llm
+  const previousMessages = await db.message.findMany({
+    where: {
+      fileId,
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 6,
+  });
+
+  // send to open ai for completion and format the messages.
+
+  const formattedPrevMessages = previousMessages.map((msg) => ({
+    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: msg.text,
+  }));
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedPrevMessages.map((message) => {
+    if (message.role === "user") return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+  
+  USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          fileId,
+          userId: user.id,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -4,12 +4,15 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { OpenAIEmbeddings } from "@langchain/openai";
 import { PineconeStore } from "@langchain/pinecone";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: "16MB" } })
+  pdfUploader: f({
+    pdf: { maxFileSize: "16MB" },
+    image: { maxFileSize: "4MB" },
+  })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = await getUser();
