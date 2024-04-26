38a7c2f3c74a0b2574a8d681a27a5ec37fdb5560
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-26 23:22:01 +0500

    chat context created for message streaming

---
+++ src/context/ChatContext.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, createContext, useRef, useState } from "react";
+import { trpc } from "@/app/_trpc/client";
+import { useToast } from "@/components/ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+
+type StreamResponse = {
+  addMessage: () => void;
+  message: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isLoading: boolean;
+};
+
+export const ChatContext = createContext<StreamResponse>({
+  addMessage: () => {},
+  message: "",
+  handleInputChange: () => {},
+  isLoading: false,
+});
+
+interface Props {
+  fileId: string;
+  children: ReactNode;
+}
+
+export const ChatContextProvider = ({ fileId, children }: Props) => {
+  const [message, setMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const utils = trpc.useUtils();
+
+  const { toast } = useToast();
+
+  // using tanstack directly here since trpc just works with json for now.
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+      if (!response.ok) throw new Error("Failed to send message");
+      return response.body;
+    },
+  });
+
+  const addMessage = () => sendMessage({ message });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        message,
+        handleInputChange,
+        isLoading,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
