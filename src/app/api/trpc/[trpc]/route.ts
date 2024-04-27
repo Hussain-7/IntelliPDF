d473e23f27aa33c8ca97c1c40ffe31f0d263b298
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-27 18:45:45 +0500

    minor updates

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,17 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/trpc";
+import { NextResponse } from "next/server";
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
+const handler = (req: Request, res: NextResponse) => {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Request-Method", "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  return fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({}),
   });
-
+};
 export { handler as GET, handler as POST };
