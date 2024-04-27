3bd06556bbff71f4826a4ddbaf47ed72b33a5c13
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-27 18:58:15 +0500

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "intellipdf-v2.vercel.app",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
