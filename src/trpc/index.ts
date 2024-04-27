0cfa259371fd0742b544ff91d1eeb48d206273ae
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-27 18:35:44 +0500

    minor updates

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -34,13 +34,21 @@ export const appRouter = router({
   }),
 
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
-    const { userId, user } = ctx;
-    const id = user.id;
-    return await db.file.findMany({
-      where: {
-        userId,
-      },
-    });
+    try {
+      const { userId, user } = ctx;
+      const id = user.id;
+      return await db.file.findMany({
+        where: {
+          userId,
+        },
+      });
+    } catch (error: any) {
+      console.log("error from getUserFiles", error.message);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message,
+      });
+    }
   }),
 
   deleteFile: privateProcedure
